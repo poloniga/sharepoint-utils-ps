5ad5cb8e948be6734b3e25e66305f949d7ca3f4c
poloniga <poloniga@gmail.com>
2017-02-17 16:16:08 +0300

    Added script to print  current navigation links

---
+++ PrintNavLinks.ps1
@@ -0,0 +1,36 @@
+if ((Get-PSSnapin "Microsoft.SharePoint.PowerShell" -ErrorAction SilentlyContinue) -eq $null)
+{
+    Add-PSSnapin "Microsoft.SharePoint.PowerShell"
+}
+
+function PrintNavLinks($siteUrl){
+
+    Try
+     {
+        $web = Get-SPWeb $siteUrl
+        $pubWeb = [Microsoft.SharePoint.Publishing.PublishingWeb]::GetPublishingWeb($web)
+
+
+
+        $qlNav = $pubWeb.Navigation.CurrentNavigationNodes
+
+        ForEach ($nav in $qlNav) 
+        {
+             $output=""+$nav.Title+ " " + $nav.Url 
+            Write-Output $output
+            ForEach ($nav2 in $nav.Children) 
+            {
+                $output="`t"+$nav2.Title+ " " + $nav2.Url 
+                 Write-Output $output
+        
+            }
+        }
+
+    }
+    Catch [system.exception]
+     {
+      "caught a system exception"
+     }
+}
+
+PrintNavLinks "site гкд"
